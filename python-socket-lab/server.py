324141d1f89308ef280db6cf3ca8c3dce5d74c8c
Ghadeer Alhayek <ghadeerhayek2001@gmail.com>
2025-09-30 18:20:54 +0100

    implemented phase1 server file

--- python-socket-lab/server.py
+++ python-socket-lab/server.py
@@ -1,10 +1,20 @@
 # python
 # Import socket library
+import socket
 # Create TCP/IP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind to localhost:8080
+s.bind(('localhost', 8080))
 # Listen for connections
+s.listen(1)
 # While True:
 # - Accept client connection
 # - Receive data from client
 # - Process data
 # - Send response back
+while True:
+    conn, addr = s.accept()
+    data = conn.recv(1024)
+    print(data)
+    conn.send(b'Hello, World!')
+    conn.close()
