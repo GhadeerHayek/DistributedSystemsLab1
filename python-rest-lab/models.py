3db1fb231f9afa2d028b235396bb757cc2454af7
Ghadeer Alhayek <ghadeerhayek2001@gmail.com>
2025-09-30 18:59:25 +0100

    Added the definition of the user class

--- python-rest-lab/models.py
+++ python-rest-lab/models.py
@@ -1,2 +1,26 @@
+"""
+    models.py contains the definition of models.
+    User Model:
+        id: int
+        name: str
+        email: str
+"""
+
 class User:
-    pass
+    def __init__(self, id, name, email):
+        # Initialize a User object
+        self.id = id
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        # String representation of a User object
+        return f'<User {self.id} {self.name} {self.email}>'
+
+    def to_dict(self):
+        # Helper function to convert a User object to a dictionary
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
