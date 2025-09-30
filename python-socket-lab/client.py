c871173ceac4a8ea798a2221ac5fe79b37f0cafb
Ghadeer Alhayek <ghadeerhayek2001@gmail.com>
2025-09-30 18:24:01 +0100

    implemented the client in phase 1

---
+++ python-socket-lab/client.py
@@ -0,0 +1,17 @@
+# the client will be connecting to the server on the designated port
+# it will send a message and print the response
+
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# the port to connect is the same as the port to bind in the server file
+client_socket.connect(('localhost', 8080))
+# the string must be encoded to bytes
+message = 'Hello, World! I am testing the socket.'
+message_encoded = message.encode('utf-8')
+client_socket.sendall(message_encoded)
+# print the response
+response = client_socket.recv(1024)
+print(response)
+# close connection
+client_socket.close()
