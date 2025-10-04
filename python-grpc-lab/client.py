760f1e8edfa6aaffbf351f36ae0719e533e81dcc
Ghadeer Alhayek <ghadeerhayek2001@gmail.com>
2025-10-04 11:18:06 +0100

    updated grpc readme file:

--- python-grpc-lab/client.py
+++ python-grpc-lab/client.py
@@ -10,12 +10,16 @@ stub = user_service_pb2_grpc.UserServiceStub(channel)
 # call to get user service, but pass a get user request object, which is generated from the proto file
 # also you should notice that by definition:
 # the get user request is made up from the id field, and it comes as the first one or it has a tag with number one
-
-get_user_response = stub.getUser(user_service_pb2.GetUserRequest(id="1"))
-print("Get user response: ", get_user_response)
-create_user_response = stub.createUser(user_service_pb2.CreateUserRequest(name="Ghadeer Test", email="ghadeerhayek@gmail.com"))
-print("Create user response: ", create_user_response)
-update_user_response = stub.updateUser(user_service_pb2.UpdateUserRequest(id="1", name="previous example", email="emailremainsthesame"))
-print("Update user response: ", update_user_response)
-delete_user_response = stub.deleteUser(user_service_pb2.DeleteUserRequest(id="1"))
-print("Delete user response: ", delete_user_response)
+try:
+    get_user_response = stub.getUser(user_service_pb2.GetUserRequest(id="1"))
+    print("Get user response: ", get_user_response)
+    create_user_response = stub.createUser(
+    user_service_pb2.CreateUserRequest(name="Ghadeer Test", email="ghadeerhayek@gmail.com"))
+    print("Create user response: ", create_user_response)
+    update_user_response = stub.updateUser(
+    user_service_pb2.UpdateUserRequest(id="1", name="previous example", email="emailremainsthesame"))
+    print("Update user response: ", update_user_response)
+    delete_user_response = stub.deleteUser(user_service_pb2.DeleteUserRequest(id="1"))
+    print("Delete user response: ", delete_user_response)
+except grpc.RpcError as e:
+    print("Error occurred: ", e.details())
