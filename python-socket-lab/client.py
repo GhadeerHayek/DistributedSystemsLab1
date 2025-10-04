bc754d4eeb541f6370a748b557ccf3fdd8736ab6
Ghadeer Alhayek <ghadeerhayek2001@gmail.com>
2025-10-04 08:33:33 +0100

    added error handling to the client in socket lab

--- python-socket-lab/client.py
+++ python-socket-lab/client.py
@@ -4,15 +4,18 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# the port to connect is the same as the port to bind in the server file
-client_socket.connect(('localhost', 8080))
-# the string must be encoded to bytes
-message = 'Hello, World! I am testing the socket.'
-message_encoded = message.encode('utf-8')
-print("trying to send a message:", message)
-client_socket.sendall(message_encoded)
-# print the response
-response = client_socket.recv(1024)
-print("server said: ", response)
-# close connection
-client_socket.close()
+try:
+    # the port to connect is the same as the port to bind in the server file
+    client_socket.connect(('localhost', 8080))
+    # the string must be encoded to bytes
+    message = 'Hello, World! I am testing the socket.'
+    message_encoded = message.encode('utf-8')
+    print("trying to send a message:", message)
+    client_socket.sendall(message_encoded)
+    # print the response
+    response = client_socket.recv(1024)
+    print("server said: ", response)
+    # close connection
+    client_socket.close()
+except ConnectionRefusedError:
+    print("Connection refused. The server is not running.")
